d870df1a74c8093f0321f0c25bab197b424db110
CatProgrammerYT <sp237752@gmail.com>
2021-03-15 16:36:05 +0300

    Edit binarySearch.py, added bubbleSorting.cpp

--- binarySearch.py
+++ binarySearch.py
@@ -1,7 +1,17 @@
+
+"""
+
+Binary search algorithm (Python)
+By CatProgrammerYT
+
+"""
+
+# Well, I can make a random generator of an array, but I`m too lazy
 array = [
 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,24,25,26,27,28,29,30,
 31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50
 ]
+# Getting the number, which user are looking for
 n = int(input('Search for the number (1-50): '))
 
 def binarySearch(array, n):
