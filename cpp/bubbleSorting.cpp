6b049c47c690765bc2862863adfa781db5ba8d41
CatProgrammerYT <sp237752@gmail.com>
2021-03-15 19:47:25 +0300

    bubbleSorting.cpp changes

--- cpp/bubbleSorting.cpp
+++ cpp/bubbleSorting.cpp
@@ -1,2 +1,31 @@
 
+/*
+
+Binary Search algorithm (C++)
+By CatProgrammerYT
+
+*/
+#include<iostream>
+
+using namespace std;
+
+int numarray[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+int n;
+
+int main() {
+
+	cout << "Enter the number, you are searching for: " << endl;
+	cin >> n;
+	
+	binarySearch(&numarray[15], n);
+	return 0;
+}
+
+void binarySearch(int* array, int number) {
+	// All stuff going here
+	
+	int len = array.lenght();
+
+
+}
 
