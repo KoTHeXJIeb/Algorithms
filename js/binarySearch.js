6005393196a03ff192ec98eb26cee4f2abf2df23
CatProgrammerYT <53112228+CatProgrammerYT@users.noreply.github.com>
2021-03-19 07:03:55 +0200

    Added binarySearch algorithm usign JavaScript

---
+++ js/binarySearch.js
@@ -0,0 +1,21 @@
+
+/*
+
+Binary Search algorithm (JavaScript)
+By CatProgrammerYT
+
+This is early build of the final file!
+
+*/
+
+var array = [124,12,4,121,12,4,125,1,63,136,138];
+
+// If there is a number in array
+var isFound;
+
+function binarySearch(array, number) {
+        return false;
+}
+
+isFound = binarySearch(array, 1);
+console.log(isFound);
